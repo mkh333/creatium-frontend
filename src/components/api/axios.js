ba409235c310cf76b3005c3cd44d9a4c50f6b4d4
mkh333 <merytv2003@gmail.com>
2026-04-06 04:54:08 +0400

    fix env variable

--- src/components/api/axios.js
+++ src/components/api/axios.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.VITE_API_URL|| "http://localhost:3100",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3100",
 });
-
-console.log("API URL:", import.meta.env.VITE_API_URL);
 
 instance.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
