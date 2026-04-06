9adf319da8c52af9192fe08e80ae0e0998af78b2
mkh333 <merytv2003@gmail.com>
2026-04-06 04:07:04 +0400

    fix api url

--- src/components/api/axios.js
+++ src/components/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3100",
 });
 
 console.log("API URL:", import.meta.env.VITE_API_URL);
